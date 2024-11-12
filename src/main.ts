507b877b015c5156e189c9b5ba0bccb9f7a69129
1000chw <1000chw@gmail.com>
2024-11-12 17:14:36 +0900

    feat: Update Swagger && cookie-parser to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Setting
+  const config = new DocumentBuilder()
+    .setTitle('Cloud Project - Nest.js')
+    .setDescription('Backend API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.use(cookieParser()); // cookie parser for auth
+
   await app.listen(3000, () => {
     console.log(`Server started on port 3000🚀`);});
 }
