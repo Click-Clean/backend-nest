34d997128b317826bbb0357b159df588805fa688
1000chw <1000chw@gmail.com>
2024-11-12 18:34:04 +0900

    Feat: ConfigModule update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService: ConfigService = app.get(ConfigService);
   // Swagger Setting
   const config = new DocumentBuilder()
     .setTitle('Cloud Project - Nest.js')
@@ -17,7 +19,10 @@ async function bootstrap() {
 
   app.use(cookieParser()); // cookie parser for auth
 
-  await app.listen(3000, () => {
-    console.log(`Server started on port 3000🚀`);});
+  const port: number = configService.get<number>('PORT') ?? 3001;
+
+  await app.listen(port, () => {
+    console.log(`Server started on port ${port}🚀`);
+  });
 }
 bootstrap();
