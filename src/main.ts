e231e359a3b9e506ef6fd28386831d0a4b83e1e7
1000chw <1000chw@gmail.com>
2024-11-20 01:46:50 +0900

    feat: Response And Exception Template

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionFilter } from './exception/all-exception.filter';
+import { AllResponseInterceptor } from './interceptors/all-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +21,11 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
   app.use(cookieParser()); // cookie parser for auth
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+
+  app.useGlobalInterceptors(new AllResponseInterceptor());
 
   app.useGlobalPipes(
     new ValidationPipe({
