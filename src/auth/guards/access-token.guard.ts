61f858f19a7aa846557a93a99ee482204084c79b
1000chw <1000chw@gmail.com>
2024-11-20 22:23:15 +0900

    feat: JWT validate logics

---
+++ src/auth/guards/access-token.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { JwtPayload } from '../jwt.payload';
+
+@Injectable()
+export class RefreshTokenGuard extends PassportStrategy(
+  Strategy,
+  'access_token',
+) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req?.cookies?.access_token;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('ACCESS_TOKEN_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload, done: VerifiedCallback): Promise<any> {
+    return done(null, payload);
+  }
+}
