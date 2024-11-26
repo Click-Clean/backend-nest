648cf00cf9996ca87bd54d186a186257199d0114
1000chw <1000chw@gmail.com>
2024-11-26 15:45:47 +0900

    feat: Article Entity

--- src/article/entities/article.entity.ts
+++ src/article/entities/article.entity.ts
@@ -1,1 +1,55 @@
-export class Article {}
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { IsDate, IsNumber, IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Article {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  @IsNumber()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  body: string;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  summary: string;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  media: string;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  author: string;
+
+  @ApiProperty()
+  @Column()
+  @IsUrl()
+  url: string;
+
+  @ApiProperty()
+  @Column()
+  @IsString()
+  category: string;
+
+  @ApiProperty()
+  @CreateDateColumn()
+  @IsDate()
+  createdAt: string;
+
+  @ApiProperty()
+  @Column()
+  @IsNumber()
+  probability: number;
+}
