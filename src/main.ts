b5c1ca483f5d0c93a37c3e31caeb25c8a52c9660
1000chw <1000chw@gmail.com>
2024-12-03 18:22:05 +0900

    docs: authorization update

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
     .setTitle('Cloud Project - Nest.js')
     .setDescription('Backend API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
